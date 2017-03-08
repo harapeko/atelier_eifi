94640f3c24183454531e311906517e82d23088c1
harapeko <tea.lile.tea@gmail.com>
2017-03-08 19:40:43 +0000

    コントローラからビューに値を渡すようにした

--- sample_app/web/controllers/static_pages_controller.ex
+++ sample_app/web/controllers/static_pages_controller.ex
@@ -5,14 +5,14 @@ defmodule SampleApp.StaticPagesController do
   # Q: connってなんぞ？
   # Q: _paramsも何がはいるんです？
   def home(conn, _params) do
-    render conn, "home.html"
+    render conn, "home.html", message: "Home"
   end
   
   def help(conn, _params) do
-    render conn, "help.html"
+    render conn, "help.html", message: "Help"
   end
   
   def about(conn, _params) do
-    render conn, "about.html"
+    render conn, "about.html", message: "About"
   end
 end
